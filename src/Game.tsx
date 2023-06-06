61a6e04c88f2a7fe11cd655c3155950cb4422b97
pranjalnarayan <pnarayan733@gmail.com>
2023-06-06 19:03:30 +0530

    1 file modified

--- src/Game.tsx
+++ src/Game.tsx
@@ -3,6 +3,7 @@ import Row from './Row';
 
 const DEFAULT_COLOR = 'red';
 const SELECTED_COLOR = 'green';
+const DELAY = 500;
 
 const Game = () => {
   const [rows, setRows] = React.useState([
@@ -10,11 +11,34 @@ const Game = () => {
     [DEFAULT_COLOR],
     [DEFAULT_COLOR, DEFAULT_COLOR, DEFAULT_COLOR],
   ]);
+  const countRef = React.useRef([]);
+
+  const checkAllCompleted = () => {
+    const totalSuares = rows.reduce((val, row) => {
+      return val + row.length;
+    }, 0);
+
+    if (totalSuares === countRef.current.length) {
+      countRef.current.forEach((square, ind) => {
+        setTimeout(() => {
+          const { rowInd, sqInd } = square;
+          const newRows = [...rows];
+          newRows[rowInd][sqInd] = DEFAULT_COLOR;
+          setRows(newRows);
+        }, DELAY * (ind + 1));
+      });
+    }
+  };
 
   const onSquareClick = React.useCallback((rowInd, sqInd) => {
     const newRows = [...rows];
     newRows[rowInd][sqInd] = SELECTED_COLOR;
     setRows(newRows);
+    countRef.current.push({
+      rowInd,
+      sqInd,
+    });
+    checkAllCompleted();
   }, []);
 
   return (
