dd1691c4133fc82a611b9681f31afb3148e63d17
pranjalnarayan <pnarayan733@gmail.com>
2023-06-07 19:50:56 +0530

    click disabled while undoing

--- src/Game.tsx
+++ src/Game.tsx
@@ -12,6 +12,7 @@ const Game = () => {
     [DEFAULT_COLOR, DEFAULT_COLOR, DEFAULT_COLOR],
   ]);
   const countRef = React.useRef([]);
+  const undoProgressRef = React.useRef(false);
 
   const checkAllCompleted = () => {
     const totalSuares = rows.reduce((val, row) => {
@@ -19,30 +20,52 @@ const Game = () => {
     }, 0);
 
     if (totalSuares === countRef.current.length) {
+      undoProgressRef.current = true;
       countRef.current.forEach((square, ind) => {
         setTimeout(() => {
           const { rowInd, sqInd } = square;
           const newRows = [...rows];
           newRows[rowInd][sqInd] = DEFAULT_COLOR;
           setRows(newRows);
-        }, DELAY * (ind + 1));
 
-        if (ind === totalSuares - 1) {
-          countRef.current = [];
-        }
+          if (ind === totalSuares - 1) {
+            countRef.current = [];
+            undoProgressRef.current = false;
+          }
+        }, DELAY * (ind + 1));
       });
     }
   };
 
-  const onSquareClick = React.useCallback((rowInd, sqInd) => {
-    const newRows = [...rows];
-    newRows[rowInd][sqInd] = SELECTED_COLOR;
-    setRows(newRows);
+  const pushStep = (rowInd, sqInd) => {
     countRef.current.push({
       rowInd,
       sqInd,
     });
     checkAllCompleted();
+  };
+
+  const removeStep = (rowInd, sqInd) => {
+    if (countRef.current && countRef.current.length) {
+      countRef.current = countRef.current.filter((square) => {
+        return !(square.rowInd === rowInd && square.sqInd === sqInd);
+      });
+    }
+  };
+
+  const onSquareClick = React.useCallback((rowInd, sqInd) => {
+    if (undoProgressRef.current) {
+      return;
+    }
+    const newRows = [...rows];
+    const isSlected = newRows[rowInd][sqInd] === SELECTED_COLOR;
+    newRows[rowInd][sqInd] = isSlected ? DEFAULT_COLOR : SELECTED_COLOR;
+    setRows(newRows);
+    if (!isSlected) {
+      pushStep(rowInd, sqInd);
+    } else {
+      removeStep(rowInd, sqInd);
+    }
   }, []);
 
   return (
